31fd6ee0269ac36cbef17d01f225820dd88e52d0
rigueti-bruno <bruno.rigueti@outlook.com>
2025-05-01 23:26:52 -0300

    materiais do periodo novo

---
+++ Periodo 4/Programação Orientada a Objetos em Java/exemplo_1.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+public class Pessoa {
+    private String nome;
+    private double codigo_identificador;
+    private Random aleatorio;
+    public Pessoa (String nome) {
+        aleatorio = new Random();
+        this.setNome(nome);
+        this.codigo_identificador = aleatorio.nextDouble();
+    }
+    private void setNome (String nome) {
+        this.nome = nome;
+    }
+    private String getNome() {
+        return this.nome;
+    }
+    public double getCodigoIdentificador() {
+        return this.codigo_identificador;
+    }
+    public static void main (String args[]) {
+        Pessoa p1 = new Pessoa("teste A");
+        System.out.println("Pessoa 1: "+p1.getNome());
+    }
+}
