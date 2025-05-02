1128ce197e441979e1994b8a69cbb8d28440f5c2
rigueti-bruno <bruno.rigueti@outlook.com>
2025-05-01 23:27:43 -0300

    arquivos do periodo novo

--- Periodo 4/Programação Orientada a Objetos em Java/Pessoa.java
+++ Periodo 4/Programação Orientada a Objetos em Java/Pessoa.java
@@ -18,7 +18,9 @@ public class Pessoa {
         return this.codigo_identificador;
     }
     public static void main (String args[]) {
-        Pessoa p1 = new Pessoa("teste A");
-        System.out.println("Pessoa 1: "+p1.getNome());
+        Pessoa p1 = new Pessoa("Teste A");
+        System.out.println("Pessoa 1: "+p1.getNome()+", Codigo Identificador: "+p1.getCodigoIdentificador());
+        Pessoa p2 = new Pessoa("Teste B");
+        System.out.println("Pessoa 2: "+p2.getNome()+", Codigo Identificador: "+p2.getCodigoIdentificador());
     }
 }
