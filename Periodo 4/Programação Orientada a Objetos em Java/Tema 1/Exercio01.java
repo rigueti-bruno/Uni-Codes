f9fef13eedd348cd4ccb94794fd238eaa2d2a102
rigueti-bruno <bruno@rigueti.com>
2025-08-23 13:53:40 -0300

    Inicio da Preparação para a Prova de POO em Java

---
+++ Periodo 4/Programação Orientada a Objetos em Java/Tema 1/Exercio01.java
@@ -0,0 +1,8 @@
+public class Exercio01 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 25;
+       int z= x + y;
+        System.out.println("x+y = " + z);
+    }
+}
